0e87ce9120362b3ae80a3a211365559561ac5eb9
Nobuyuki Ochi <n-oochi@ksw.co.jp>
2026-01-09 20:01:30 +0900

    mainに変数部分を追加

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -30,3 +30,27 @@ provider "aws" {
   profile = "terraform"
   region  = "us-east-1"
 }
+
+# ---------------------------------------------
+# Variables
+# ---------------------------------------------
+variable "project" {
+  type    = string
+  default = "tastylog"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+# variable "username" {
+#   type      = string
+#   default   = "admin"
+#   sensitive = true
+# }
+
+# variable "password" {
+#   type      = string
+#   sensitive = true
+# }
